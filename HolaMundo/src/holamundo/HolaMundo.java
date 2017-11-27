286be64954394c377952e5094714bbfc93a56ff5
Estudiante <Estudiante@lab0300>
2017-11-27 07:50:01 +0100

    ESTA ES LA PRIMERA VERSION CON INTERNACIONALIZACION EN INGLES , PROXIMAMENTE EN FRANCES

---
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundo;
+
+import java.util.ResourceBundle;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ResourceBundle rB;
+        rB=ResourceBundle.getBundle("holamundo.EN");
+        System.out.println(rB.getString("etiqueta1"));
+    }
+    
+}
